b64d7d4f6e4c100ac7474937f3652065f452940a
gomeow <davismariotti@gmail.com>
2012-12-22 23:36:04 -0800

    Made simple file writing php script.

--- post.php
+++ post.php
@@ -1,9 +1,20 @@
 <html>
 <body>
+<?php
+$author = $_POST["author"];
+$title = $_POST["title"];
+$content = $_POST["content"];
+?>
+Welcome <?php echo $author; ?>!<br>
+Book Title: <?php echo $title; ?>
+Content: <?php echo $content; ?> 
 
-Welcome <?php echo $_POST["author"]; ?>!<br>
-Book Title: <?php echo $_POST["title"]; ?>
-Content: <?php echo $_POST["content"]; ?> 
 
+<php
+$file = fopen("books/" . $author . "-" . $title, "w");
+fwrite($file, "!" . $author . "\n!" . $title . "\n" . $content);
+fclose($file);
+echo "File Written!";
+?>
 </body>
 </html>
